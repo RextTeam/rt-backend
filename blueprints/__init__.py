ce03de2b3dda4b782a9a4149080e28c6d7a9b160
tasuren <tasuren_0618@yahoo.co.jp>
2022-07-24 11:40:58 +0900

    update: 新RTのURL埋め込み作成機能に対応した。

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -1,11 +1,23 @@
 # RT - Blueprints
 
-from sanic import Blueprint
+from sanic import Blueprint, html
+
+from core import Request
 
 from .captcha import bp as captcha_bp
 from .oauth import bp as oauth_bp
-from .short_url import bp as short_url_bp
+from .short_url import bp as short_url_bp # type: ignore
 from .payments import bp as payments_bp
 
 
-bpg = Blueprint.group(captcha_bp, oauth_bp, short_url_bp, payments_bp)
+bp = Blueprint("general")
+
+
+@bp.route("/embed")
+async def short_url(request: Request):
+    return html(await request.app.ctx.tempylate.aiorender_from_file(
+        f"rt-frontend/pages/embed.html", data=request.args
+    ))
+
+
+bpg = Blueprint.group(bp, captcha_bp, oauth_bp, short_url_bp, payments_bp)
